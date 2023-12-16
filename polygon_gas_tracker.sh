9f9462b15ce6d9f8b58bf95cfb29ceba6b71bf4e
Lytves <22356770+lytves@users.noreply.github.com>
2023-12-16 22:57:12 +0100

    Create polygon gas tracker

---
+++ polygon_gas_tracker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# <xbar.title>Polygon Gas Price Tracker</xbar.title>
+# <xbar.version>v2.0.1</xbar.version>
+# <xbar.author>lytves</xbar.author>
+# <xbar.author.github>lytves</xbar.author.github>
+# <xbar.desc>Polygon GasPrice forecast system.</xbar.desc>
+# <xbar.image></xbar.image>
+# <xbar.abouturl>https://docs.polygonscan.com/</xbar.abouturl>
+#
+#Polygon GasPrice forecast system based on PolygonScan Developer APIs
+
+content=$(curl -s "https://api.polygonscan.com/api?module=gastracker&action=gasoracle&apikey=YOUR_API_KEY" | /usr/local/bin/jq --raw-output '.result.SafeGasPrice') && echo "♾️ $content | color=purple"
